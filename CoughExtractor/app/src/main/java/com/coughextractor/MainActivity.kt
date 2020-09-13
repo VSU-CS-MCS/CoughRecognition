86bd3b16ecdcf1c3a3de959b2c9e02cc3c798f06
Pavel Levchuk <spaierinstinct@gmail.com>
2020-09-13 14:16:07 +0300

    refactor(CoughExtractor): use ComponentActivity instead of AppCompatActivity

--- CoughExtractor/app/src/main/java/com/coughextractor/MainActivity.kt
+++ CoughExtractor/app/src/main/java/com/coughextractor/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.coughextractor
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
